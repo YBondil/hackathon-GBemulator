98d5522518469f4f7426ea104174005d4014a27b
Arthur Marchier <arthur.marchier@gmail.com>
2026-06-29 15:55:54 +0200

    init ecran

--- lib/utils/definitions.hpp
+++ lib/utils/definitions.hpp
@@ -8,3 +8,10 @@ using u8 = uint8_t ;
 using u16 = uint16_t ;
 using s8 = int8_t ;
 using s16 = int16_t ;
+
+
+Struct Brique {
+    int Height;
+    int Width;
+    int *Full; //On note les blocs pleins de la brique
+}
