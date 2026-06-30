2d7b2d53509f37f4087930d4aed8ffb1f9ee8a80
Valentin-Tomoki Roy-Matsui <valentin-tomoki.roy@etu.minesparis.psl.eu>
2026-06-30 15:23:55 +0200

    avancé opcodes

--- src/CPU/CPU_instructions.cpp
+++ src/CPU/CPU_instructions.cpp
@@ -1,6 +1,8 @@
-#include "CPU.hpp"
+#include "CPU/CPU.hpp"
+#include "bitwise.hpp"
 
 /*ADC*/
+
 Cycles CPU::_opcode_adc(u8 value){
     u8 reg = A.value();
     u8 carry = F.flag_carry_value();
@@ -12,23 +14,28 @@ Cycles CPU::_opcode_adc(u8 value){
     F.set_flag_subtract(false);
     F.set_flag_half_carry((reg & 0xf) + (value & 0xf) + carry > 0xf);
     F.set_flag_carry(result_tot > 0xff);
+
     A.set(result);
 
     PC += 1;             //décalage de 1 byte
-
+    
     Cycles cycles(1);
     return cycles;
 
 }
+
+
 Cycles CPU::opcode_adc_HL(){
     u8 value = memory.read(HL.value());
     _opcode_adc(value);
 
     PC += 1;
-
+    
     Cycles cycles(2);
     return cycles;
 }
+
+
 Cycles CPU::opcode_adc_n8(u8 value){
     _opcode_adc(value);
 
@@ -57,7 +64,7 @@ Cycles CPU::_opcode_add(u8 reg, u8 value){
 }
 
 
-Cycles CPU::opcode_add_HL(){
+Cycles CPU::opcode_add_A_HL(){
     u8 value = memory.read(HL.value());
     u8 reg = memory.read(PC);
     _opcode_add(value,reg);
@@ -69,9 +76,11 @@ Cycles CPU::opcode_add_HL(){
 }
 
 
-Cycles CPU::opcode_add_n8(u8 value, u8 reg){
+Cycles CPU::opcode_add_A_n8(u8 value, u8 reg){
     _opcode_add(value,reg);
 
+    Cycles cycles(2);
+    return cycles;
 }
 
 
@@ -81,7 +90,7 @@ Cycles CPU::opcode_add_n8(u8 value, u8 reg){
 
 /*Loadouts*/
 
-Cycles CPU::_opcode_ld(Register R1, Register R2){
+Cycles CPU::_opcode_ld(Register& R1, Register& R2){
     R2.set(R1.value());
 
     PC += 1;
@@ -91,7 +100,7 @@ Cycles CPU::_opcode_ld(Register R1, Register R2){
 }
 
 
-Cycles CPU::opcode_ld_n8(u8 n, Register R){
+Cycles CPU::opcode_ld_n8(u8 n, Register& R){
     R.set(n);
 
     PC += 2;
@@ -101,7 +110,7 @@ Cycles CPU::opcode_ld_n8(u8 n, Register R){
 }
 
 
-Cycles CPU::opcode_ld_n16(u16 n, RegisterPair R){
+Cycles CPU::opcode_ld_n16(u16 n, RegisterPair& R){
     R.set(n);
 
     PC += 3;
@@ -111,7 +120,7 @@ Cycles CPU::opcode_ld_n16(u16 n, RegisterPair R){
 }
 
 
-Cycles CPU::opcode_ld_HL(Register R){
+Cycles CPU::opcode_ld_HL(Register& R){
     memory.write(HL.value(), R.value());
 
     PC += 1;
@@ -121,7 +130,7 @@ Cycles CPU::opcode_ld_HL(Register R){
 }
 
 
-Cycles CPU::opcode_ld_HL_n8(RegisterPair R){
+Cycles CPU::opcode_ld_HL_n8(RegisterPair& R){
     memory.write(HL.value(), R.value()); 
       
     PC += 2;
@@ -131,7 +140,7 @@ Cycles CPU::opcode_ld_HL_n8(RegisterPair R){
 }
 
 
-Cycles CPU::opcode_ld_HL_r8(Register R){
+Cycles CPU::opcode_ld_HL_r8(Register& R){
     R.set(memory.read(HL.value()));
 
     PC += 1;
@@ -142,7 +151,7 @@ Cycles CPU::opcode_ld_HL_r8(Register R){
 }
 
 
-Cycles CPU::opcode_ld_A(RegisterPair R){
+Cycles CPU::opcode_ld_A(RegisterPair& R){
     memory.write(R.value(), A.value());
 
     PC += 1;
@@ -183,7 +192,7 @@ Cycles CPU::opcode_ldh_C_A(){
 }
 
 
-Cycles CPU::opcode_ld_A_r16(RegisterPair R){
+Cycles CPU::opcode_ld_A_r16(RegisterPair& R){
     A.set(memory.read(R.value()));
 
     PC += 1;
@@ -194,7 +203,7 @@ Cycles CPU::opcode_ld_A_r16(RegisterPair R){
 
 
 Cycles CPU::opcode_ld_A_n16(const u16 adress){
-    A.set(adress);
+    A.set(memory.read(adress));
 
     PC += 3;
 
@@ -278,8 +287,8 @@ Cycles CPU::opcode_ld_SP_n16(const u16 n){
 
 
 Cycles CPU::opcode_ld_n16_SP(const u16 adress){
-    u8 down = SP.value() & 0xFF;
-    u8 up = SP.value() >> 8;
+    u8 down = SP.low();
+    u8 up = SP.high();
     memory.write(adress, down);
     memory.write(adress + 1, up);
 
@@ -305,4 +314,124 @@ Cycles CPU::opcode_ld_HL_SP_s8(s8 n){
 
     Cycles cycles(3);
     return cycles;
+}
+
+
+Cycles CPU::opcode_SP_HL(){
+    SP.set(HL.value());
+
+    PC += 1;
+
+    Cycles cycles(2);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_nop(){
+    PC += 1;
+
+    Cycles cycles(1);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_or_A_r8(Register& R){
+    u8 R_value = R.value();
+    u8 A_value = A.value();
+    u8 result = R_value || A_value;
+    A.set(result);
+
+    F.set_flag_zero(result==0);
+    F.set_flag_subtract(false);
+    F.set_flag_half_carry(false);
+    F.set_flag_carry(false);
+
+    Cycles cycles(2);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_or_A_HL(){
+    u8 value = memory.read(HL.value());
+    u8 A_value = A.value();
+    u8 result = value || A_value;
+    A.set(result);
+
+    F.set_flag_zero(result==0);
+    F.set_flag_subtract(false);
+    F.set_flag_half_carry(false);
+    F.set_flag_carry(false);
+
+    Cycles cycles(2);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_or_A_n8(u8 n){
+    u8 A_value = A.value();
+    u8 result = n || A_value;
+    A.set(result);
+
+    F.set_flag_zero(result==0);
+    F.set_flag_subtract(false);
+    F.set_flag_half_carry(false);
+    F.set_flag_carry(false);
+
+    Cycles cycles(2);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_pop_AF(){
+    u16 value = pop_stack();
+    u8 low = static_cast<u8>(value) ;
+    AF.set(value);
+
+    F.set_flag_zero(bitwise::check_bit(value, 7));
+    F.set_flag_subtract(bitwise::check_bit(value,6));
+    F.set_flag_half_carry(bitwise::check_bit(value,5));
+    F.set_flag_carry(bitwise::check_bit(value,4));
+
+    Cycles cycles(3);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_pop_r16(RegisterPair& R){
+    u16 value = pop_stack();
+    u8 low = static_cast<u8>(value) ;
+    R.set(value);
+
+    Cycles cycles(3);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_push_AF(){
+    push_stack(AF.value());
+
+    Cycles cycles(4);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_push_r16(RegisterPair& R){
+    push_stack(R.value());
+
+    Cycles cycles(4);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_res_u3_r8(Register& R){
+    u8 value = R.value();
+    R.set(value &= 0XF8);
+
+    Cycles cycles(2);
+    return cycles;
+}
+
+
+Cycles CPU::opcode_res_u3_HL(){
+
 }
