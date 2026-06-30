31bce6c5795a2088cae7188e03b7b6e41cf0aaac
Aymar <aymarsmy@gmail.com>
2026-06-30 16:49:41 +0200

    update memory.cpp et .hpp, version pas encore testée

--- lib/memory.hpp
+++ lib/memory.hpp
@@ -8,26 +8,26 @@
 
 
 class Memory {
-    public : 
-    std::vector<u8> ROM_00 = std::vector<u8>(16384);
-    std::vector<u8> ROM_01 = std::vector<u8>(16384);
-    std::vector<u8> VRAM = std::vector<u8>(8192);
-    std::vector<u8> Ext_RAM = std::vector<u8>(8192);
-    std::vector<u8> WRAM_1 = std::vector<u8>(4096);
-    std::vector<u8> WRAM_2 = std::vector<u8>(4096);
-    std::vector<u8> Echo_RAM = std::vector<u8>(7680);
-    std::vector<u8> OAM = std::vector<u8>(144);
-    // Ajouter message d'erreur si essaie entrer dans zone FEA0 - FEFF (interdit)
-    std::vector<u8> IO = std::vector<u8>(112);
-    std::vector<u8> HRAM = std::vector<u8>(111);
-    std::vector<u8> Interrupt = std::vector<u8>(1);
+    private : 
+   
+    std::vector<u8> WRAM;
+    std::vector<u8> OAM;
+    std::vector<u8> HRAM;
     
-    
+
+    public:
+
+    Memory();
 
     u8 read(u16 adress);
-    void write(u16 adress, u8 x);
+    auto read_io(u16 address); 
+    auto unmapped_io_read(u16 address);
 
+    void write(const u16 adress, const u8 data);
+    void write_io(const u16 address, const u8 data);
+    void unmapped_io_write(const u16 address, const u8 data);
 
+    bool boot_rom_active();
 };
 
 
