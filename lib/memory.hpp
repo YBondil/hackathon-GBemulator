d73fc5478ea2aef20060daf57f812b74c75a7105
Aymar <aymarsmy@gmail.com>
2026-06-30 09:34:55 +0200

    memory public

--- lib/memory.hpp
+++ lib/memory.hpp
@@ -8,24 +8,24 @@
 
 
 class Memory {
-
-    std::vector<u8> ROM_00;
-    std::vector<u8> ROM_01;
-    std::vector<u8> VRAM;
-    std::vector<u8> Ext_RAM;
-    std::vector<u8> WRAM_1;
-    std::vector<u8> WRAM_2;
-    std::vector<u8> Echo_RAM;
-    std::vector<u8> OAM;
+    public : 
+    std::vector<u8> ROM_00 = std::vector<u8>(16384);
+    std::vector<u8> ROM_01 = std::vector<u8>(16384);
+    std::vector<u8> VRAM = std::vector<u8>(8192);
+    std::vector<u8> Ext_RAM = std::vector<u8>(8192);
+    std::vector<u8> WRAM_1 = std::vector<u8>(4096);
+    std::vector<u8> WRAM_2 = std::vector<u8>(4096);
+    std::vector<u8> Echo_RAM = std::vector<u8>(7680);
+    std::vector<u8> OAM = std::vector<u8>(144);
     // Ajouter message d'erreur si essaie entrer dans zone FEA0 - FEFF (interdit)
-    std::vector<u8> IO;
-    std::vector<u8> HRAM;
-    std::vector<u8> Interrupt;
+    std::vector<u8> IO = std::vector<u8>(112);
+    std::vector<u8> HRAM = std::vector<u8>(111);
+    std::vector<u8> Interrupt = std::vector<u8>(1);
     
-    Memory::Memory();
+    
 
-    u8 Memory::read(u16 adress);
-    void Memory::write(u16 adress);
+    u8 read(u16 adress);
+    void write(u16 adress, u8 x);
 
 
 };
--- lib/utils/definitions.hpp
+++ lib/utils/definitions.hpp
@@ -2,7 +2,7 @@
 
 
 #include <cstdint>
-
+#include <iostream>
 
 using u8 = uint8_t ;
 using u16 = uint16_t ;
@@ -10,6 +10,8 @@ using s8 = int8_t ;
 using s16 = int16_t ;
 
 
-int u16_into_int(u16 adress){
-    
+
+template <typename T>
+void affiche(T x){
+    std::cout << x << std::endl;
 }
