8d8c455e5061a5ecbc555f268f7171f65834318b
Arthur Marchier <arthur.marchier@gmail.com>
2026-06-30 17:33:08 +0200

    C'est à peu près clean mais il reste encore pas mal à faire

--- src/affichage/PPU.cpp
+++ src/affichage/PPU.cpp
@@ -5,9 +5,8 @@ int FillImage(FrameBuffer& buffer) -> void {
     int image[GB_WIDTH * GB_HEIGHT];
     for (int y = 0; y < GB_HEIGHT; y++) {
         for (int x = 0; x < GB_WIDTH; x++) {
-            Color color = buffer.get_pixel(x, y);
-            int pixel_value = get_color(u8 pixel_value)
-            image[y * GB_WIDTH + x] = pixel_value;
+            image[y * GB_WIDTH + x] = buffer.get_pixel(x, y);
         }
     }
+    return image;
 }
--- src/affichage/colors.hpp
+++ src/affichage/colors.hpp
@@ -1,3 +1,5 @@
 #include "def_colors.hpp"
 
+//Pas sûr de l'utilité de cette fonction, n'hésite pas à la supprimer si pas utile
+
 void get_color(u8 pixel_value) -> GBColor;
