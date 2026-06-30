e15551c860141e986e1c863b93538b06e21b4c49
Arthur Marchier <arthur.marchier@gmail.com>
2026-06-30 11:28:24 +0200

    def des couleurs

--- affichage/include/briques.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-Struct Brique {
-    int Height;
-    int Width;
-    int *Full; //On note les blocs pleins de la brique
-}
--- lib/utils/definitions.hpp
+++ lib/utils/definitions.hpp
@@ -8,10 +8,3 @@ using u8 = uint8_t ;
 using u16 = uint16_t ;
 using s8 = int8_t ;
 using s16 = int16_t ;
-
-
-Struct Brique {
-    int Height;
-    int Width;
-    int *Full; //On note les blocs pleins de la brique
-}
